cd74c6c32d2d54ab41cd9568a6f3dd79dc0789d7
Thomas Lasky <Lasky@ThomastekiMac-mini.local>
2015-03-25 08:32:11 +0800

    Initial Commit

---
+++ 2simpletable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2simpletable
+//
+//  Created by Thomas Lasky on 15/3/25.
+//  Copyright (c) 2015年 Thomas Lasky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
