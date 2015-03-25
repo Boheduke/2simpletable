60593ea1b6f2c0192b2b55b2c9eebd4d0785157d
Thomas Lasky <Lasky@ThomastekiMac-mini.local>
2015-03-25 09:43:07 +0800

    Initial state

--- 2simpletable/ViewController.swift
+++ 2simpletable/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    var restaurantNames=["Cafe Deadend","Homei","Teakha","Cafe Loisl","Petite Oster","For Kee Restaurant","Po's Atelier","Bourke Street Bakery","Haigh's Chocolate","Palmomino Espresso","Upstrate","Traif","Graham Avenue Meats And Deli" ]
+    // use a array to store the table data.
+    
+    }
 
+func tableView (tableView: UITableView, numberOfRowsInsection section :Int) -> Int {
+    // Return the number of rows in the section.
+    return restaurantNames.count
 }
+
+
+func tableView ( tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+    
+    let cellIdentifier = "Cell"
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier,forIndexPath:indexPath) as UITableViewCell
+    
+    //config the cell...\
+    cell.textLabel?.text=restaurantNames[indexPath.row]
+    return cell
+}
+
+
 
